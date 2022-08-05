1a6882a60364e5426503a63bd109f9d1e359d644
damien th <damienthery@yahoo.fr>
2022-08-05 15:38:09 +0200

    remove ok

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -108,5 +108,5 @@ exports.deleteUser = (req, res, next) => {
        })
        .catch(error => res.status(400).json({ error }));
     })
-    .catch(error => res.status(500).json({ error: 'okkk' }));
+    .catch(error => res.status(500).json({ error }));
 };
